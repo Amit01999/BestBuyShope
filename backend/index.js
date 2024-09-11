f27f7f0146d38860411895c6e128d0d4ee1c1c4a
Amit Kumar Bishwas <135952879+Amit01999@users.noreply.github.com>
2024-09-12 01:39:32 +0600

    Update index.js
    cors update

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,9 @@ const app = express();
 app.use(
   cors({
     origin: 'https://best-buy-shope-krgy.vercel.app/',
-    methods: 'GET, POST, PUT, DELETE',
+     methods: 'GET, POST, PUT, DELETE',
     credentials: true,
+     allowedHeaders: 'Content-Type, Authorization',
   })
 );
 app.use(express.json());
