5cbdc011c822b92e5b1a3475e493587182770d88
Amit01999 <amitsarkar019999@gmail.com>
2024-09-12 00:26:38 +0600

    eror solve

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const router = require('./routes');
 const app = express();
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL,
     methods: 'GET,POST,PUT,DELETE',
     credentials: true,
   })
