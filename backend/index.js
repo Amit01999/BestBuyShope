3112ba204ca68a31e0df5fb7656d9a9c0eae5374
Amit01999 <amitsarkar019999@gmail.com>
2024-09-12 20:22:29 +0600

    backend error solve

--- backend/index.js
+++ backend/index.js
@@ -9,14 +9,9 @@ const app = express();
 app.use(
   cors({
     origin: 'https://best-buy-shope-krgy.vercel.app/',
-<<<<<<< HEAD
-    methods: 'GET,POST,PUT,DELETE',
+    methods: 'GET, POST, PUT, DELETE',
     credentials: true,
-=======
-     methods: 'GET, POST, PUT, DELETE',
-    credentials: true,
-     allowedHeaders: 'Content-Type, Authorization',
->>>>>>> 94583212532c48cffdd96c65d5d6c1ae5d9bedb2
+    allowedHeaders: 'Content-Type, Authorization',
   })
 );
 app.use(express.json());
