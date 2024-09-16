3a8258152e13abefbf05eb37d815ebe03eb18ee5
Amit01999 <amitsarkar019999@gmail.com>
2024-09-17 02:45:16 +0600

    addToCart controller code modifie

--- backend/controller/user/addToCartController.js
+++ backend/controller/user/addToCartController.js
@@ -2,22 +2,29 @@ const addToCartModel = require('../../models/cartProduct');
 
 const addToCartController = async (req, res) => {
   try {
-    console.log(' add to cart  hit');
     const { productId } = req?.body;
-    const currentUser = req.userId;
+    const currentUser = req.userId; // Assuming req.userId contains the current user's ID
 
-    const isProductAvailable = await addToCartModel.findOne({ productId });
+    // Check if the product is already in the cart for the current user
+    const isProductAvailable = await addToCartModel.findOne({
+      productId: productId,
+      userId: currentUser,
+    });
 
-    console.log('isProductAvailabl   ', isProductAvailable);
+    console.log(
+      'add to cart controller isProductAvailable:',
+      isProductAvailable
+    );
 
     if (isProductAvailable) {
       return res.json({
-        message: 'Already exits in Add to cart',
+        message: 'Already exists in Add to Cart',
         success: false,
         error: true,
       });
     }
 
+    // If not, add the product to the cart
     const payload = {
       productId: productId,
       quantity: 1,
@@ -29,7 +36,7 @@ const addToCartController = async (req, res) => {
 
     return res.json({
       data: saveProduct,
-      message: 'Product Added in Cart',
+      message: 'Product Added to Cart',
       success: true,
       error: false,
     });
